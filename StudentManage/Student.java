b8fc9da611b9f09123e721cc0f0f6a0bc11ebbad
G-Prasath <guruprasathmsc@gmail.com>
2026-09-13 20:38:51 +0530

    Done for StudentManage File

--- StudentManage/Student.java
+++ StudentManage/Student.java
@@ -6,60 +6,69 @@ public class Student {
     private String name;
     private int age;
     private String course;
-    private double mark;
+    private double marks;
 
     // Constructer
-    public Student(int id, String name, int age, String course, double mark){
+    public Student(int id, String name, int age, String course, double marks) {
         this.id = id;
         this.name = name;
         this.age = age;
         this.course = course;
-        this.mark = mark;
+        this.marks = marks;
     }
 
     // GETTERS
 
-    public int getId(){
+    public int getId() {
         return id;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public int getAge(){
+    public int getAge() {
         return age;
     }
 
-    public String getCourse(){
+    public String getCourse() {
         return course;
     }
 
-    public double getMark(){
-        return mark;
+    public double getmarks() {
+        return marks;
     }
 
     // SETTERS
-    public void setId(int id){
+    public void setId(int id) {
         this.id = id;
     }
 
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
     }
 
-    public void setAge(int age){
+    public void setAge(int age) {
         this.age = age;
     }
 
-    public void setCourse(String course){
+    public void setCourse(String course) {
         this.course = course;
     }
 
-    public void setMark(double mark){
-        this.mark = mark;
+    public void setmarks(double marks) {
+        this.marks = marks;
     }
 
-    
+    // Display Student Details
+    public void displayStudents() {
+        System.out.println("------------------------------");
+        System.out.println("Student ID   : " + id);
+        System.out.println("Student Name : " + name);
+        System.out.println("Age          : " + age);
+        System.out.println("Course       : " + course);
+        System.out.println("markss        : " + marks);
+        System.out.println("------------------------------");
+    }
 
 }
--- StudentManage/StudentManage.java
+++ StudentManage/StudentManage.java
@@ -1,5 +1,90 @@
 package StudentManage;
 
+import java.util.*;
+
 public class StudentManage {
-    
+    // Create List of Students
+    private List<Student> students;
+
+    // Constructors
+    public StudentManage() {
+        students = new ArrayList<>();
+    }
+
+    // add Student
+    public void addStudent(Student student) {
+        // Check if Already Existes
+        for (Student s : students) {
+            if (s.getId() == student.getId()) {
+                System.out.println("Student ID already exists!");
+                return;
+            }
+        }
+
+        students.add(student);
+        System.out.println("Student added successfully!");
+    }
+
+    // view all Students
+    public void viewStudents() {
+        if (students.isEmpty()) {
+            System.out.println("No students found!");
+            return;
+        }
+
+        System.out.println("\n========== STUDENT LIST ==========");
+
+        for (Student s : students) {
+            s.displayStudents();
+        }
+    }
+
+    // Search Studes
+    public void searchStudent(int id) {
+        for (Student s : students) {
+            if (s.getId() == id) {
+                System.out.println("\nStudent found!");
+                s.displayStudents();
+                return;
+            }
+        }
+        System.out.println("Student not found!");
+    }
+
+    // Update Student
+    public void updateStudent(
+            int id,
+            String name,
+            int age,
+            String course,
+            double marks) {
+
+        for (Student s : students) {
+            if (s.getId() == id) {
+                s.setId(id);
+                s.setName(name);
+                s.setAge(age);
+                s.setCourse(course);
+                s.setmarks(marks);
+
+                System.out.println("Student updated successfully!");
+                return;
+            }
+        }
+        System.out.println("Student not found!");
+
+    }
+
+    // Delete Student
+    public void deleteStudent(int id) {
+        for (Student s : students) {
+            if (s.getId() == id) {
+                students.remove(s);
+                System.out.println("Student deleted successfully!");
+                return;
+            }
+        }
+        System.out.println("Student not found!");
+    }
+
 }
