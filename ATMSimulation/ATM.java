03d56754537e987b6f1901a900c2ce26567b80b5
G-Prasath <guruprasathmsc@gmail.com>
2026-09-23 13:16:34 +0530

    Done for Atm Manager file

--- ATMSimulation/ATM.java
+++ ATMSimulation/ATM.java
@@ -15,7 +15,6 @@ public class Atm {
     }
 
     // Getters
-
     public int getAccountNumber() {
         return accountNumber;
     }
--- ATMSimulation/Atmmanager.java
+++ ATMSimulation/Atmmanager.java
@@ -2,11 +2,32 @@ package ATMSimulation;
 
 import java.util.*;
 
-public class ATMmanager{
-    List<Atm> atmDB;
+public class ATMManager{
+    private Atm atm;
     
-    public ATMmanager(){
-        atmDB = new ArrayList<>();
+    public ATMManager(Atm atm){
+       this.atm = atm;
+    }
+
+    // Login
+    public boolean login(int enteredPin){
+        if(enteredPin == atm.getPin()){
+            System.out.println("Login successful.");
+            return true;
+        }
+
+        System.out.println("Incorrect PIN.");
+        return false;
+    }
+
+    // Check Balance
+    public void checkBalance(){
+        atm.checkBalance();
+    }
+
+    // deposite
+    public void deposite(int amount){
+        atm.deposite();
     }
 
     
