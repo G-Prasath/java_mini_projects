c9209a6e04f4d3c1e54eac5e2dcb1963fb84bc60
G-Prasath <guruprasathmsc@gmail.com>
2026-09-23 12:43:30 +0530

    Done for ATM java file

--- ATMSimulation/ATM.java
+++ ATMSimulation/ATM.java
@@ -1,6 +1,6 @@
 package ATMSimulation;
 
-public class ATM {
+public class Atm {
 
     // Initialize the account number, pin, and balance
     private int accountNumber;
@@ -8,7 +8,7 @@ public class ATM {
     private double balance;
 
     // Constructor to initialize the account number, pin, and balance
-    public ATM(int accountNumber, int pin, double balance){
+    public Atm(int accountNumber, int pin, double balance) {
         this.accountNumber = accountNumber;
         this.pin = pin;
         this.balance = balance;
@@ -29,10 +29,60 @@ public class ATM {
     }
 
     // Setters
-
-    public void setPin(int pin){
+    public void setPin(int pin) {
         this.pin = pin;
     }
 
+    // Deposite
+    public void deposite(double amount) {
+        if (amount <= 0) {
+            System.out.println("Invalid deposit amount.");
+            return;
+        }
 
+        balance = balance + amount;
+        System.out.println("Amount deposited: ₹" + amount);
+        System.out.println("New balance: ₹" + balance);
+    }
+
+    // Widthdraw
+    public void withdraw(double amount) {
+        if (amount <= 0) {
+            System.out.println("Invalid widthdraw amount.");
+            return;
+        }
+
+        if (amount > balance) {
+            System.out.println("Insufficient balance.");
+            return;
+        }
+
+        balance = balance - amount;
+        System.out.println("Amount Withdraw: ₹" + amount);
+        System.out.println("New balance: ₹" + balance);
+    }
+
+    // Check Balance
+    public void checkBalance() {
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Current Balance: ₹" + balance);
+    }
+
+    // Change PIN
+    public boolean changePin(int oldPin, int newPin) {
+        if (this.pin != oldPin) {
+            System.out.println("Old PIN is incorrect.");
+            return false;
+        }
+
+        if (newPin < 1000 || newPin > 9999) {
+            System.out.println("PIN must contain 4 digits.");
+            return false;
+        }
+
+        this.pin = newPin;
+
+        System.out.println("PIN changed successfully.");
+        return true;
+    }
 }
---
+++ ATMSimulation/Atmmanager.java
@@ -0,0 +1,14 @@
+package ATMSimulation;
+
+import java.util.*;
+
+public class ATMmanager{
+    List<Atm> atmDB;
+    
+    public ATMmanager(){
+        atmDB = new ArrayList<>();
+    }
+
+    
+   
+}   
